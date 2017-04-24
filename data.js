5321264aaaf2be464bbc68cd4d125b2e6f317b3a
hudsonmacedo <hudsonmacedo14@gmail.com>
2017-04-24 20:31:33 -0300

    finished first try wth vue.js

---
+++ data.js
@@ -0,0 +1,58 @@
+new Vue({
+    el: '#app',
+    data: {
+        // HEADER
+        title:
+        'Educação aproxima seus horizontes.',
+        content:
+        'O Vivo Educa tem mais de 200 cursos à sua disposição, classificados entre 8 categorias, para aprender no seu tempo.',
+        headerIMG:
+        'Telas do aplicativo Vivo Educa',
+        
+        // BOTÃO
+        ctaBtn1: 'Quero começar a aprender',
+        ctaURL: '#', // vamos mandar pra um link ou pra parte da página?
+
+        // Features do produto
+        featuresTitle:
+        'Mais que aulinhas',
+        
+        firstItemTitle:
+        'Dicas de carreira',
+        firstItemContent:
+        'Tenha aulas preparatórias para entrevistas de emprego com os cursos de carreira do e melhore suas entrevistas de emprego.',
+        
+        secondItemTitle:
+        'Cozinha e gastronomia',
+        secondItemContent:
+        'Gostaria de saber cozinhar melhor ou melhorar o que você já sabe? Aqui você consegue aprender com cursos básicos e avançados.',
+        thirdItemTitle:
+        'Aprenda tecnologia',
+        thirdItemContent:
+        'Em pleno século 21 não se pode mais ficar sem conhecer o mínimo da tecnologia que nos cerca e entendê-las melhor.',
+
+        //Como funciona?
+        comoTitle:
+        'Como funciona?',
+        // Falar como assina o produto, outras vantagens que não citou anteriormente e como se usa
+        como1:
+        'O Vivo Educa pode ser assinado aqui neste site ou no aplicativo. Assinando e baixando o aplicativo, você vai ter acesso a todos os cursos da nossa plataforma, exercícios, materiais de apoio, além de ter cursos ao vivo e poder acessar todos os vídeos offline usando seu smartphone ou tablet.',
+        // Informações de valores e assinaturas
+        como2:
+        'O valor do serviço é de R$ 4,99/semana ou de R$12,99/mês (desconto de 13%) mas você também tem a opção de acessar todas as aulas gratuitas pelo tempo até encontrar o curso que vai querer!',
+
+        //Último Call To Action - CTA
+        ctaTitle:
+        'Quer aprender mais?',
+        ctaBtn2: 
+        'Quero começar a aprender!',
+
+        //Footer
+        ctaFooter:
+        'Baixe agora:',
+        appStore:
+        'https://itunes.apple.com/br/app/vivo-educa/id1158949850?ls=1&mt=8', // Colocar o link da App Store ou deixar vazio
+        playStore:
+        'https://play.google.com/store/apps/details?id=com.movile.learning.vivo' // Colocar o link da Play Store ou deixar vazio
+    }
+});
