537ecab6b1705be4a50e79018f5b3c78849af5a2
hudsonmacedo <hudsonmacedo14@gmail.com>
2017-07-19 09:45:00 -0300

    going back commit

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ new Vue({
     data: {
         // HEADER
         title:
-        'Educação te ajuda a crescer sempre!',
+        'Educação aproxima seus horizontes!',
         content:
         'O Vivo Educa tem mais de 200 cursos à sua disposição, classificados entre 8 categorias, para aprender no seu tempo.',
         headerIMG:
